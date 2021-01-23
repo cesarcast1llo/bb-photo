5df91dacb2c2dc9c2d10b515a3054b7b72244471
cesarcast1llo <cesarcast1llo@yahoo.com>
2021-01-22 22:06:34 -0800

    firebase set up, styling needed, storage ready

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import { firebase } from '@firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyBnU7_s6iIzVq3X1zD6yArPLFB5Vf0p-YA',
+  authDomain: 'bb-photo.firebaseapp.com',
+  projectId: 'bb-photo',
+  storageBucket: 'bb-photo.appspot.com',
+  messagingSenderId: '745724473332',
+  appId: '1:745724473332:web:a5004b681faf7ee13353a1',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
