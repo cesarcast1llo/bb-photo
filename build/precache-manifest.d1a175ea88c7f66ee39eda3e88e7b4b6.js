d8c590c314422c88b5fbb7f85200272a80e6076b
cesarcast1llo <cesarcast1llo@yahoo.com>
2021-01-22 22:22:02 -0800

    targeting build folder for heroku push

---
+++ build/precache-manifest.d1a175ea88c7f66ee39eda3e88e7b4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d472ef401b9ec461e175631e32f8f423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2738f34a61e1192e00a",
+    "url": "/static/css/main.0d1e748b.chunk.css"
+  },
+  {
+    "revision": "21f5086ef2b9d4da3bf3",
+    "url": "/static/js/2.69a95de7.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.69a95de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2738f34a61e1192e00a",
+    "url": "/static/js/main.33e8f725.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
