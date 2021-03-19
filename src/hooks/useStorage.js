9afacc66b35478fa168c673c246129f8302ca4d8
cesarcast1llo <cesarcast1llo@yahoo.com>
2021-03-18 23:12:37 -0700

    setting up firebase storage

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -10,5 +10,22 @@ const useStorage = (file) => {
   useEffect(() => {
     //   references
     const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on(
+      'state_changed',
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadUrl();
+        setUrl(url);
+      }
+    );
   }, [file]);
+  return { progress, url, error };
 };
+export default useStorage;
